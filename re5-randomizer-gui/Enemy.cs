a98065bdc6e6a477e18e4fc6b772bbdb25168660
MissehsJune <34357113+missehsjune@users.noreply.github.com>
2025-09-04 07:59:43 -0500

    Add project files.

---
+++ re5-randomizer-gui/Enemy.cs
@@ -0,0 +1,12 @@
+﻿namespace re5_randomizer_gui
+    {
+    public class Enemy
+        {
+        public string gameID;
+        public string name;
+        public string xmlData;
+        public string txtData;
+        public int minLife;
+        public int maxLife;
+        }
+    }
---
+++ re5-randomizer-gui/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace re5_randomizer_gui
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonFolderSelect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelFilePath = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.labelSeedNumber = new System.Windows.Forms.Label();
+            this.buttonRandomize = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.labelEnemyVariety = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelMapSelection = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.labelEnemySelection = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // buttonFolderSelect
+            // 
+            this.buttonFolderSelect.Location = new System.Drawing.Point(493, 36);
+            this.buttonFolderSelect.Name = "buttonFolderSelect";
+            this.buttonFolderSelect.Size = new System.Drawing.Size(87, 23);
+            this.buttonFolderSelect.TabIndex = 0;
+            this.buttonFolderSelect.Text = "Select Folder";
+            this.buttonFolderSelect.UseVisualStyleBackColor = true;
+            this.buttonFolderSelect.Click += new System.EventHandler(this.buttonFolderSelect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(475, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelFilePath
+            // 
+            this.labelFilePath.AutoSize = true;
+            this.labelFilePath.Location = new System.Drawing.Point(13, 19);
+            this.labelFilePath.Name = "labelFilePath";
+            this.labelFilePath.Size = new System.Drawing.Size(229, 13);
+            this.labelFilePath.TabIndex = 2;
+            this.labelFilePath.Text = "Select location of game files and ARCTool.exe:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(212, 69);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2147483647,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // labelSeedNumber
+            // 
+            this.labelSeedNumber.AutoSize = true;
+            this.labelSeedNumber.Location = new System.Drawing.Point(12, 71);
+            this.labelSeedNumber.Name = "labelSeedNumber";
+            this.labelSeedNumber.Size = new System.Drawing.Size(194, 13);
+            this.labelSeedNumber.TabIndex = 4;
+            this.labelSeedNumber.Text = "Enter a seed number for randomization: ";
+            // 
+            // buttonRandomize
+            // 
+            this.buttonRandomize.Enabled = false;
+            this.buttonRandomize.Location = new System.Drawing.Point(269, 206);
+            this.buttonRandomize.Name = "buttonRandomize";
+            this.buttonRandomize.Size = new System.Drawing.Size(75, 23);
+            this.buttonRandomize.TabIndex = 5;
+            this.buttonRandomize.Text = "Randomize";
+            this.buttonRandomize.UseVisualStyleBackColor = true;
+            this.buttonRandomize.Click += new System.EventHandler(this.buttonRandomize_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(170, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Single enemy per spawn group";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // labelEnemyVariety
+            // 
+            this.labelEnemyVariety.Location = new System.Drawing.Point(13, 99);
+            this.labelEnemyVariety.Name = "labelEnemyVariety";
+            this.labelEnemyVariety.Size = new System.Drawing.Size(91, 13);
+            this.labelEnemyVariety.TabIndex = 0;
+            this.labelEnemyVariety.Text = "Enemy Variety: ";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(161, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Match original game\'s variety";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(16, 115);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 76);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // labelMapSelection
+            // 
+            this.labelMapSelection.AutoSize = true;
+            this.labelMapSelection.Location = new System.Drawing.Point(203, 99);
+            this.labelMapSelection.Name = "labelMapSelection";
+            this.labelMapSelection.Size = new System.Drawing.Size(112, 13);
+            this.labelMapSelection.TabIndex = 9;
+            this.labelMapSelection.Text = "Levels to Randomize: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Location = new System.Drawing.Point(206, 115);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(184, 76);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(7, 42);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(173, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "All non-guaranteed crash levels";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(7, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(99, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Only safe levels";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // labelEnemySelection
+            // 
+            this.labelEnemySelection.AutoSize = true;
+            this.labelEnemySelection.Location = new System.Drawing.Point(393, 99);
+            this.labelEnemySelection.Name = "labelEnemySelection";
+            this.labelEnemySelection.Size = new System.Drawing.Size(98, 13);
+            this.labelEnemySelection.TabIndex = 11;
+            this.labelEnemySelection.Text = "Enemy List to Use: ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton6);
+            this.groupBox3.Controls.Add(this.radioButton5);
+            this.groupBox3.Location = new System.Drawing.Point(396, 115);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(184, 76);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(6, 42);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(78, 17);
+            this.radioButton6.TabIndex = 1;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "All enemies";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Checked = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(111, 17);
+            this.radioButton5.TabIndex = 0;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Only safe enemies";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 250);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.labelEnemySelection);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.labelMapSelection);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelEnemyVariety);
+            this.Controls.Add(this.buttonRandomize);
+            this.Controls.Add(this.labelSeedNumber);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.labelFilePath);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonFolderSelect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Resident Evil 5 Randomizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonFolderSelect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelFilePath;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label labelSeedNumber;
+        private System.Windows.Forms.Button buttonRandomize;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label labelEnemyVariety;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelMapSelection;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label labelEnemySelection;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        }
+    }
+
